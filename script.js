7e92bfe3a4f5c7c2c902a9a6b6c63a58001f87be
Camryn <camrynpatterson@yahoo.com>
2024-03-22 14:42:06 -0500

    adding functions that cause the to do data inputted by user to stay upon refresh of the page

--- script.js
+++ script.js
@@ -15,12 +15,27 @@ function addItem() {
         li.appendChild(span);
     }
     textBox.value = "";
+    saveList();
 }
 
+
+// Check marked tasks and delete if clicked
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveList();
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-    }
+        saveList();
+        }
 }, false);
+
+// Keep list populated when page is refreshed
+function saveList() {
+    localStorage.setItem("list", listContainer.innerHTML);
+}
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("list");
+}
+showList();
